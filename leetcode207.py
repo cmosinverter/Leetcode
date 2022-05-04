fda8bcadcc624e4e8f6bb1dac7711b34f4770219
kevin95120 <61655399+kevin95120@users.noreply.github.com>
2022-05-04 22:57:12 +0800

    Create leetcode207.py

---
+++ leetcode207.py
@@ -0,0 +1,30 @@
+class Solution:
+    from collections import deque
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        graph = [[] for i in range(numCourses)]
+        indeg = [0]*numCourses
+        
+        for item in prerequisites:
+            u = item[0]
+            v = item[1]
+            
+            graph[v].append(u)
+            indeg[u] += 1
+        
+        q = deque([])
+        for i in range(numCourses):
+            if indeg[i] == 0:
+                q.append(i)
+        
+        while q:
+            u = q.popleft()
+            
+            for v in graph[u]:
+                indeg[v] -= 1
+                if indeg[v] == 0:
+                    q.append(v)
+                    
+                    
+        return sum(indeg) == 0
+        
